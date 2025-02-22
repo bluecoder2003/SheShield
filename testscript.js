236a9db5cceb31c004bd6856cb4487c8fb9ba646
IndraniSom <indranisom39@gmail.com>
2025-02-22 11:11:48 +0530

    backend pushed

--- testscript.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcryptjs";
-
-const testPassword = "123456Ui@"; // Replace with your actual password
-const hashedPassword = await bcrypt.hash(testPassword, 10);
-
-console.log("Hashed Password:", hashedPassword);
-
-const isMatch = await bcrypt.compare(testPassword, hashedPassword);
-console.log("Password Match Result:", isMatch); // Should be true
